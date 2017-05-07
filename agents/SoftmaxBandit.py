03195689e9672d877fef7c9ef42befe96b09cbd1
akshay narayan <okkhoy@gmail.com>
2017-05-07 22:49:13 +0800

    add update function to softmax bandit agent

--- agents/SoftmaxBandit.py
+++ agents/SoftmaxBandit.py
@@ -78,3 +78,20 @@ class Softmax:
         z = sum([math.exp(v/self.temperature) for v in self.values])
         probs = [math.exp(v/self.temperature)/z for v in self.values]
         return categorical_draws(probs)
+
+    def update(self, chosen_arm, reward):
+        """
+        Increments count of the chosen arm.
+        Determines the current estimated value of the chosen arm.
+            Updates the estimated value of the chosen arm to be a weighted average of the
+            previously estimated value and the reward just received.
+            Update rule actually counts the running average
+        :param chosen_arm: arm that was selected to be pulled (action to be performed)
+        :param reward: numerical value obtained for performing the action
+        """
+        self.counts[chosen_arm] += 1
+        n = self.counts[chosen_arm]
+
+        value = self.values[chosen_arm]
+        new_value = ((n-1)/float(n)) * value + (1/float(n)) * reward
+        self.values[chosen_arm] = new_value
