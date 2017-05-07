927f0100e0f0dd9e71f2168362c03bac1b8157aa
akshay narayan <okkhoy@gmail.com>
2017-05-07 15:22:38 +0800

    add bernoulli random action generator

--- environments/BernoulliEnvironment.py
+++ environments/BernoulliEnvironment.py
@@ -28,3 +28,17 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
+
+import random
+
+
+class BernoulliArm:
+
+    def __init__(self, p):
+        self.p = p
+
+    def draw(self):
+        if random.random() > self.p:
+            return 0.0
+        else:
+            return 1.0
